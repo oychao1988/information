14a9dcdc95a70235316c2b6c17a9e1b021a3fa61
oychao <2296458423@qq.com>
2018-08-24 21:28:41 +0800

    抽取config类到config.py 创建DevelopConfig类 创建ProductionConfig类

---
+++ config.py
@@ -0,0 +1,28 @@
+import redis
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+    REIDS_NUM = 1
+
+    SECRET_KEY = 'oychao'
+
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    SESSION_PERMANENT = 86400 * 2
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    'develepment': DevelopmentConfig,
+    'production': ProductionConfig
+}
--- manage.py
+++ manage.py
@@ -6,32 +6,17 @@ import redis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 from flask_migrate import Migrate, MigrateCommand
-
+from config import config
 app = Flask(__name__)
-class Config(object):
-    DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-    REIDS_NUM = 1
-
-    SECRET_KEY = 'oychao'
-
-    SESSION_TYPE = 'redis'
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    SESSION_USE_SIGNER = True
-    SESSION_PERMANENT = 86400 * 2
-
-app.config.from_object(Config)
+app.config.from_object(config['develepment'])
 
 manager = Manager(app)
 db = SQLAlchemy(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REIDS_NUM)
+redis_store = redis.StrictRedis(host=config['develepment'].REDIS_HOST, port=config['develepment'].REDIS_PORT, db=config['develepment'].REIDS_NUM)
 csrf = CSRFProtect(app)
 Session(app)
 
