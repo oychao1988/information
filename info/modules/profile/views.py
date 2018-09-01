70239b0e64312b5d92577da28bafab956082d6ae
oychao <2296458423@qq.com>
2018-09-01 15:13:44 +0800

    添加修改用户基本资料功能

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,14 +1,55 @@
-from flask import g
+from flask import current_app
+from flask import g, jsonify
 from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
 
+from info import db
 from info.utils.common import login_user_info
+from info.utils.response_code import RET
 from . import profile_bp
 
-@profile_bp.route('/info')
+@profile_bp.route('')
 @login_user_info
 def user_info():
     user = g.user
     data = {
-        'user': user.to_dict() if user else []
+        'user_info': user.to_dict() if user else []
     }
     return render_template('user/user.html', data=data)
+
+@profile_bp.route('/base_info', methods=['GET', 'POST'])
+@login_user_info
+def base_info():
+    user = g.user
+    # 检查用户是否登录
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg='用户未登录')
+
+    if request.method == 'GET':
+        return render_template('user/user_base_info.html', data={'user_info': user.to_dict()})
+
+    # 获取参数
+    params_dict = request.json
+    signature = params_dict.get('signature')
+    nick_name = params_dict.get('nick_name')
+    gender = params_dict.get('gender')
+    print(params_dict)
+    if not signature and not nick_name and not gender:
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不足')
+
+    if signature:
+        user.signature = signature
+    if nick_name:
+        user.nick_name = nick_name
+    if gender in ['MAN', 'WOMAN']:
+        user.gender = gender
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据库修改用户资料失败')
+    session['nick_name'] = user.nick_name
+    return jsonify(errno=RET.OK, errmsg='修改用户资料成功', data={'user_info': user.to_dict()})
--- info/static/news/js/user_base_info.js
+++ info/static/news/js/user_base_info.js
@@ -10,16 +10,42 @@ $(function () {
 
         var signature = $("#signature").val()
         var nick_name = $("#nick_name").val()
-        var gender = $(".gender").val()
+        var gender = $(".gender:checked").val()
 
-        if (!nick_name) {
-            alert('请输入昵称')
-            return
-        }
-        if (!gender) {
-            alert('请选择性别')
-        }
+        // if (!nick_name) {
+        //     alert('请输入昵称')
+        //     return
+        // }
+        // if (!gender) {
+        //     alert('请选择性别')
+        // }
 
         // TODO 修改用户信息接口
+        var params = {
+            'signature': signature,
+            'nick_name': nick_name,
+            'gender': gender
+        }
+        $.ajax({
+            url: '/user/base_info',
+            type: 'post',
+            data: JSON.stringify(params),
+            contentType: 'application/json',
+            dataType: 'json',
+            headers: {'X-CSRFToken': getCookie('csrf_token')},
+            success: function(resp){
+                if(resp.errno==0){
+                    $('.user_center_name', parent.document).html(params['nick_name'])
+                    $('#nick_name', parent.document).html(params['nick_name'])
+                    $('.input_sub').blur()
+                }else{
+                    if(resp.errno=4101){
+                        $('.login_form_con').show()
+                    }else{
+                        console.log(resp.errmsg)
+                    }
+                }
+            }
+        })
     })
 })
